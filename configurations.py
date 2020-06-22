de93624a5796fa681d58dd6ba81658289fefd2c8
singularity014 <49594316+singularity014@users.noreply.github.com>
2020-06-22 15:08:02 +0200

    Update configurations.py

--- configurations.py
+++ configurations.py
@@ -2,11 +2,11 @@
 For adding all the configurations
 and variables expected in creating Neural Nets
 '''
-
+# -----------------------
 MAX_SEQUENCE_LENGTH = 100
 MAX_VOCAB_SIZE = 20000
 EMBEDDIN_DIM = 100
 VALIDATION_SPLIT = 0.2
 BATCH_SIZE = 128
 EPOCH = 10
-
+# -----------------------
